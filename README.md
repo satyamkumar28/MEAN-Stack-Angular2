a31294cb8f88c949d825df2fa05df285d36eab42
vikash singh <vikash@apiria.com>
2018-01-22 02:20:08 +0530

    create mean stack application using angular 2. 1.create node server. 2.connect database 3.create client angular 2 4.create route 5.create navbar component 6.create dashboard component

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MEAN-Stack-Angular2
+->install express
+->install mongoose
+    1.create config file -> database.js
+           -> database - using node js crypto.randomBytes api to generate secret key
+    2.Install Angular 2 cli
+    3.create new app client
+    4.cd client/  ng build
+    5.index.js (join path to client/dist/index.html)
+
+    Client Folder->
+    1.angular 2 router (Routing - Navigation -ts - Guide - angular.io)
+
+
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+
+import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

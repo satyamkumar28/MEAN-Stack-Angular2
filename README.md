7bd536c0b7d280135cbfe8259fa28ad6ae1b09b7
vikash singh <vikash@apiria.com>
2018-01-23 01:52:53 +0530

    create mean stack application using angular 2. 1.create models -> user schema 2.create routes -> authentication 3.install bycrpt for encrypt password 4.install body-parser 5.create post api for register the users

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # MEAN-Stack-Angular2
-->install express
-->install mongoose
-    1.create config file -> database.js
-           -> database - using node js crypto.randomBytes api to generate secret key
+1.install express
+2.install mongoose
+
+    1.create config file => database.js
+           database - using node js crypto.randomBytes api to generate secret key
     2.Install Angular 2 cli
     3.create new app client
     4.cd client/  ng build
@@ -10,5 +11,10 @@
 
     Client Folder->
     1.angular 2 router (Routing - Navigation -ts - Guide - angular.io)
-
+    2.create routes => authentication.js file
+    3.create models => create user schema
+    4.install body parse
+    5.install bycrpt for encrypt password.
+    6.create a customs validators for email, username and password using regExpression
+    7.create /authentication/register post api and check postman.
 
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -13,12 +17,19 @@ mongoose.connect(config.uri, (err) => {
     }
 });
 
+// Provider static directory for frontend
+app.use(bodyParser.urlencoded({extend:false}));
+//parser application/Form
+app.use(bodyParser.json());
 app.use(express.static(__dirname+'/client/dist/'));
+app.use('/authentication',authentication);
 
+// Connect server to Angular 2 Index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/dist/index.html'));
 });
 
+//Start Server: Listen on port 3000
 
 app.listen(3000, () => {
     console.log('Start server')
